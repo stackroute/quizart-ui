d6172757db929f9e4c16730ed19ff7f51c2af037
kirtijalan <kirtijalan194@gmail.com>
2016-11-28 10:21:14 +0530

    fixed issues : gamePlay,Timer,styles

--- client/components/GamePlay.jsx
+++ client/components/GamePlay.jsx
@@ -1,305 +1,181 @@
-  import React from 'react';
-  import ReactDOM from 'react-dom';
-  import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-  import FlatButton from 'material-ui/FlatButton';
-  import {Grid, Row, Col} from 'react-flexbox-grid';
-  import RaisedButton from 'material-ui/RaisedButton';
-  import {blue300} from './../../node_modules/material-ui/styles/colors';
-  import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-  import Avatar from 'material-ui/Avatar';
-  import Paper from 'material-ui/Paper';
-  import {List, ListItem, makeSelectable} from 'material-ui/List';
-  import Subheader from 'material-ui/Subheader';
-  import {red500, deepOrange400, teal500, blue500, grey900,cyan50,redA400,limeA700,pinkA200} from './../../node_modules/material-ui/styles/colors';
-  import Divider from 'material-ui/Divider';
-  import TimerSpeed from './timer';
-  import {Link} from 'react-router';
-  import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-var color='#1A237E';
-  const style={
-    width: '90%', margin: '2px',textAlign:'center',
-    backgroundColor:color,color:'white',
-
-  }
-
-  var res=0;
-  var qstn_no=0;
-  var quiz=[
-  {
-    "question": "For which of the following disciplines is Nobel Prize awarded?",
-    "options": [
-    "Physics and Chemistry",
-    "Physiology or Medicine",
-    "Literature, Peace and Economics",
-    "All of the above"
-    ],
-    "currect": "All of the above",
-    "image":"http://res.cloudinary.com/deaxb0msww/image/upload/v1480077584/nobelprize_nf4q0k.png",
-    "timelimit":"15000"
-  },
-  {
-    "question": "WWW stands for?",
-    "options": [
-    "World Whole Web",
-    "Wide World Web",
-    "Web World Wide",
-    "World Wide Web"
-    ],
-    "currect": "World Wide Web",
-    "image":"http://res.cloudinary.com/deaxb0msww/image/upload/v1480077581/www_j80fke.png",
-    "timelimit":"16000"
-
-  },
-  {
-    "question": "Which of the following are components of Central Processing Unit (CPU) ?",
-    "options": [
-    "Arithmetic logic unit, Mouse",
-    "Arithmetic logic unit, Control unit",
-    "Arithmetic logic unit, Integrated Circuits",
-    "Control Unit, Monitor"
-    ],
-    "currect": "Arithmetic logic unit, Control unit",
-    "image":null,
-    "timelimit":"15000"
-  },
-  {
-    "question": "Who are you?",
-    "options": [
-    "A",
-    "B",
-    "C",
-    "All of the above"
-    ],
-    "currect": "B",
-    "image":"http://res.cloudinary.com/deaxb0msww/image/upload/v1480077577/whoareu_zcfufc.png",
-    "timelimit":"15000"
-  },
-  {
-    "question": "For which of the following disciplines is Nobel Prize awarded?",
-    "options": [
-    "Physics and Chemistry",
-    "Physiology or Medicine",
-    "Literature, Peace and Economics",
-    "All of the above"
-    ],
-    "currect": "All of the above",
-    "image":"http://res.cloudinary.com/deaxb0msww/image/upload/v1480077577/cpu_g58w7k.png",
-    "timelimit":"15000"
-  },
-  {
-   "question": "WWW stands for?",
-   "options": [
-   "World Whole Web",
-   "Wide World Web",
-   "Web World Wide",
-   "World Wide Web"
-   ],
-   "currect": "World Wide Web",
-   "image":"http://res.cloudinary.com/deaxb0msww/image/upload/v1480077581/www_j80fke.png",
-   "timelimit":"15000"
-
- },
- {
-  "question": "Which of the following are components of Central Processing Unit (CPU) ?",
-  "options": [
-  "Arithmetic logic unit, Mouse",
-  "Arithmetic logic unit, Control unit",
-  "Arithmetic logic unit, Integrated Circuits",
-  "Control Unit, Monitor"
-  ],
-  "currect": "Arithmetic logic unit, Control unit",
-  "image":"http://res.cloudinary.com/deaxb0msww/image/upload/v1480077577/cpu_g58w7k.png",
-  "timelimit":"15000"
-},
-{
- "question": "WWW stands for?",
- "options": [
- "World Whole Web",
- "Wide World Web",
- "Web World Wide",
- "World Wide Web"
- ],
- "currect": "World Wide Web",
- "image":"http://res.cloudinary.com/deaxb0msww/image/upload/v1480077581/www_j80fke.png",
- "timelimit":"15000"
-
-}
-]
-var i=0;
-var timer=0;
-var qstn_len=quiz.length;
-var tem=quiz[1].timelimit/15;
+import React from 'react';
+ import ReactDOM from 'react-dom';
+ import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+ import FlatButton from 'material-ui/FlatButton';
+ import {Grid, Row, Col} from 'react-flexbox-grid';
+ import RaisedButton from 'material-ui/RaisedButton';
+ import {blue300} from './../../node_modules/material-ui/styles/colors';
+ import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+ import Avatar from 'material-ui/Avatar';
+ import Paper from 'material-ui/Paper';
+ import {List, ListItem, makeSelectable} from 'material-ui/List';
+ import Subheader from 'material-ui/Subheader';
+ import {red500, deepOrange400, teal500, blue500, grey900,cyan50,redA400,limeA700,pinkA200} from './../../node_modules/material-ui/styles/colors';
+ import Divider from 'material-ui/Divider';
+ import superagent from 'superagent';
+ // import appbar from './appbar';
+ import TimerSpeed from './Timer';
+ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+ <link rel="stylesheet" href="./styles/StyleProj.css"/>
+ var color='#1A237E';
+var res=0;
+var crtAnswer=[];
+var timeLimit=1;
+var optionArray=[];
+var numberOfQuestions=8;
 var blocks = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-/*-----------------------------------------Adaptive Challenge Creation---------------------------------------------------*/
-
-
-class GamePlay extends React.Component
-{
- constructor(props) {
-   super(props);
-
-   this.state = {open: false,i: 0,result:0,timelimit:0,qstnno:0,result:0,temp:quiz[1].timelimit/15,opacity:1,color4:'#1A237E',color3:'#1A237E',color2:'#1A237E',color1:'#1A237E',able:false};
-   this.handleClick = this.handleClick.bind(this);
+ /*-----------------------------------------Adaptive Challenge Creation---------------------------------------------------*/
+ class GamePlay extends React.Component
+ {
+  constructor(props) {
+    super(props);
+    this.state = {topic:[],limit:8,open: false,result:0,timeLimit:0,questionNumber:0,opacity:1,color4:'#1A237E',color3:'#1A237E',color2:'#1A237E',color1:'#1A237E',able:false};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  /*-----------------------------------------handdling event List------------------------------------------------------*/
+  handleClick(text,value) {
+ var val=text;
+ var ans=crtAnswer[this.state.questionNumber];
+ this.setState( {able:true,opacity:0.8} );
+ var j=0,c=0;
+ for(var k=0;k<4;k++)
+ {
+   if(optionArray[this.state.questionNumber][k]==ans)
+   {
+     c=k+1;
+   }
  }
-
-
-
- /*-----------------------------------------handdling event List------------------------------------------------------*/
-
- handleClick(text,value) {
-    var val=text;
-   var ans=quiz[this.state.qstnno].currect;
-   tem=quiz[this.state.qstnno].timelimit/15;
-   this.setState( {temp:tem,able:true,opacity:0.8} );
-   var j=0,c=0;
-   for(var k=0;k<4;k++)
-   {
-    if(quiz[this.state.qstnno].options[k]==ans)
-    {
-      c=k+1;
-    }
+/*-------------Changing colour of Correct option----------------------------------------*/
+ if(c==1)
+ {
+   this.setState({color1:'#8BC34A'});
+ }
+ else if(c==2)
+ {
+   this.setState({color2:'#8BC34A'});
+ }
+ else if(c==3)
+ {
+   this.setState({color3:'#8BC34A'});
+ }
+ else if(c==4)
+ {
+   this.setState({color4:'#8BC34A'});
+ }
+/*---------Changing colour of InCorrect option--------------------------------------------*/
+ if(value==1)
+ {
+   if(ans===val){
+    this.setState( {result:this.state.result+1,color1:'#8BC34A'});
+  }
+  else
+  {
+    this.setState( {color1:'#F44336'});
+  }
    }
-
-
-   if(c==1)
-   {
-      this.setState({color1:'#8BC34A'});
-   }
-   else if(c==2)
-   {
-    this.setState({color2:'#8BC34A'});
-   }
-    else if(c==3)
-   {
-    this.setState({color3:'#8BC34A'});
-   }
-    else if(c==4)
-   {
-    this.setState({color4:'#8BC34A'});
-   }
-   if(value==1)
-   {
-      if(ans.toUpperCase()===val.toUpperCase()){
-     this.setState( {result:this.state.result+1,color1:'#8BC34A'});
-   }
-   else
-   {
-         this.setState( {color1:'#F44336'});
-
-   }
-   }
-   else if(value==2){
-    if(ans.toUpperCase()===val.toUpperCase()){
+ else if(value==2){
+   if(ans===val){
      this.setState( {result:this.state.result+1,color2:'#8BC34A'});
    }
    else
    {
-         this.setState( {color2:'#F44336'});
-
+     this.setState( {color2:'#F44336'});
    }
-   }
-   else if(value==3){
-    if(ans.toUpperCase()===val.toUpperCase()){
+ }
+ else if(value==3){
+   if(ans===val){
      this.setState( {result:this.state.result+1,color3:'#8BC34A'});
    }
    else
    {
-         this.setState( {color3:'#F44336'});
-
+     this.setState( {color3:'#F44336'});
    }
-   }
-   else if(value==4){
-    if(ans.toUpperCase()===val.toUpperCase()){
+ }
+ else if(value==4){
+   if(ans===val){
      this.setState( {result:this.state.result+1,color4:'#8BC34A'});
    }
    else
    {
-         this.setState( {color4:'#F44336'});
-
+     this.setState( {color4:'#F44336'});
    }
-   }
-
-}
- /*-----------------------------------------handdling event Next------------------------------------------------------*/
-
- handleChange(e) {
-
-   this.setState( {qstnno:this.state.qstnno+1,i:0});
-   qstn_no+=1;
-   tem=quiz[this.state.qstnno].timelimit/15;
-   this.setState( {color1:'#1A237E',color2:'#1A237E',color3:'#1A237E',color4:'#1A237E',able:false,opacity:1});
-
-       ReactDOM.unmountComponentAtNode(document.getElementById('root'));
-    ReactDOM.render(<MuiThemeProvider><TimerSpeed timelimit={quiz[this.state.qstnno+1].timelimit} blocks={e}/>
-    </MuiThemeProvider>, document.getElementById('root'));
-
  }
-
-
- /*-----------------------------------------componentDidMount------------------------------------------------------*/
-
+}
+/*-----------------------------------------handdling event Next------------------------------------------------------*/
+handleChange(e) {
+this.setState( {questionNumber:this.state.questionNumber+1});
+this.setState( {color1:'#1A237E',color2:'#1A237E',color3:'#1A237E',color4:'#1A237E',able:false,opacity:1});
+}
+/*-----------------------------------------componentDidMount------------------------------------------------------*/
 componentDidMount() {
-  timer=quiz[this.state.qstnno].timelimit/15;
-
-  this.setState({timelimit:quiz[this.state.qstnno].timelimit});
-  this.timerID = setInterval(
-    () => this.tick(),
-    this.state.temp
-    );
-
- /*-----------------------------------------componentWillMount------------------------------------------------------*/
-
+timeLimit=(timeLimit*(60*1000))/15;
+this.setState({timeLimit: timeLimit});
+superagent
+.get('http://localhost:3000/topic?_limit='+this.state.limit)
+.end((err, res) => {
+ this.setState({topic: res.body});
+});
 }
+/*-----------------------------------------componentWillMount------------------------------------------------------*/
 componentWillUnmount() {
-  clearInterval(this.timerID);
+return 0;
 }
-
-
-
- /*-----------------------------------------tick function------------------------------------------------------*/
-
-tick() {
- i=this.state.i;
-  if(this.state.i==15)
-  {
-    this.setState({
-      i:0,timelimit:quiz[this.state.qstnno].timelimit,qstnno:this.state.qstnno+1,opacity:1
-    });
-    qstn_no+=1;
-     ReactDOM.unmountComponentAtNode(document.getElementById('root'));
-    ReactDOM.render(<MuiThemeProvider><TimerSpeed timelimit={quiz[this.state.qstnno].timelimit} blocks={[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]}/>
-    </MuiThemeProvider>, document.getElementById('root'));
-       this.setState( {color1:'#1A237E',color2:'#1A237E',color3:'#1A237E',color4:'#1A237E',color:'white',able:false});
-
-  }
-  else
-  {
-    this.setState({
-      i:i+1,timelimit:this.state.timelimit+1
-    });
-  }
-}
-
-
-
- /*-----------------------------------------render function------------------------------------------------------*/
-
+/*-----------------------------------------render function------------------------------------------------------*/
 render()
 {
-  return(
-   <center>
-   <Grid>
-   <Row>
-   <Col xs={12} sm={12} md={12} lg={12}>
+/*-------------------------------------Fetching Images Fron array topic---------------------------*/
+
+const TopicImage = this.state.topic ? this.state.topic.map((topics) => {
+ return (
+   <CardMedia
+   overlay={<div id="root"><TimerSpeed timeLimit={this.state.timeLimit} blocks={blocks}/></div>} >
+   <img src={topics.image} style={{height:"260px",width:'80%'}}/>
+   </CardMedia>
+ );
+}) : null;
+/*--------------------------------------Fetching Questions Fron array topic-------------------------*/
+const TopicQuestions = this.state.topic ? this.state.topic.map((topics) => {
+ return (
+ <Col xs={9} sm={10} md={10} lg={10} className="Questions">
+  <h4>{this.state.questionNumber+1}) {topics.question}</h4>
+ </Col>
+ );
+}) : null;
+/*--------------------------------------Fetching Correct Options Fron array topic--------------------*/
+const TopicCorrect = this.state.topic ? this.state.topic.map((topics) => {
+ return(
+   topics.correctOption
+ );
+}) : null;
+crtAnswer=TopicCorrect;
+/*--------------------------------------Fetching Options Fron array topic-----------------------------*/
+const TopicOptions = this.state.topic ? this.state.topic.map((topics) => {
+ return (
+    <div className="Options">
+      <ListItem  primaryText={topics.options[0]}  onTouchTap={() => this.handleClick(topics.options[0],1)} disabled={this.state.able} style={{width: '90%', margin: '2px',textAlign:'center',backgroundColor:this.state.color1,color:'white',borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',opacity:this.state.opacity}}/>
+      <ListItem  primaryText={topics.options[1]}  onTouchTap={() => this.handleClick(topics.options[1],2)} disabled={this.state.able} style={{width: '90%', margin: '2px',textAlign:'center',backgroundColor:this.state.color2,color:'white',borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',opacity:this.state.opacity}} />
+      <ListItem  primaryText={topics.options[2]}  onTouchTap={() => this.handleClick(topics.options[2],3)} disabled={this.state.able} style={{width: '90%', margin: '2px',textAlign:'center',backgroundColor:this.state.color3,color:'white',borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',opacity:this.state.opacity}} />
+      <ListItem  primaryText={topics.options[3]}  onTouchTap={() => this.handleClick(topics.options[3],4)} disabled={this.state.able} style={{width: '90%', margin: '2px',textAlign:'center',backgroundColor:this.state.color4,color:'white',borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',opacity:this.state.opacity}} />
+    </div>
+  );
+}) : null;
+const TopicOptionsForCheck = this.state.topic ? this.state.topic.map((topics) => {
+ return (
+  topics.options
+  );
+}) : null;
+optionArray=TopicOptionsForCheck;
+ return(
+  <center>
+  <Grid>
+  <Row>
+  <Col xs={12} sm={12} md={12} lg={12}>
    <Paper style={{height:"100%",width: '90%',textAlign: 'center',display: 'inline-block',padding:5,paddingLeft:10,margin:5,backgroundColor:'#F5F5F5'}} zDepth={1}>
-   <center>
-   <Row style={{height:"100%",textAlign: "justify",textJustify: "interWord"}}>
-   <Col xs={9} sm={10} md={10} lg={10}>
-   <h4 style={{color:"#1A237E",margin:20,paddingLeft:'20%',textAlign:'center'}}>{this.state.qstnno+1}) {quiz[this.state.qstnno].question}</h4>
-  </Col>
+  <center>
+  <Row style={{height:"100%",textAlign: "justify",textJustify: "interWord"}}>
+  {TopicQuestions[this.state.questionNumber]}
   <Col xs={2} sm={2} md={2} lg={2}>
-  <Avatar style={{padding:20,position:'relative',backgroundColor:"white"}} size={30}><h2 style={{color:'#1A237E'}}>{this.state.result}/{qstn_len}</h2>	</Avatar>
+  <Avatar style={{padding:15,position:'relative',backgroundColor:"#1A237E"}} size={30}><h3 style={{color:'white'}}>{this.state.result}/{numberOfQuestions}</h3> </Avatar>
   </Col>
   </Row>
   <Divider/>
@@ -309,22 +185,13 @@ render()
   <Col xs={12} sm={10} md={10} lg={6}>
   <center>
   <Card style={{width:'90%',height:260,margin:10,backgroundColor:'#F5F5F5'}}>
-  <CardMedia
-  overlay={<div id="root"><TimerSpeed timelimit={quiz[this.state.qstnno].timelimit} blocks={blocks}/></div>} >
-  <ImageFun/>
-  </CardMedia>
+  {TopicImage[this.state.questionNumber]}
   </Card>
   </center>
   <Subheader>Choose your answer</Subheader>
-
-
-
-  <ListItem  primaryText={quiz[this.state.qstnno].options[0]}  onTouchTap={() => this.handleClick(quiz[this.state.qstnno].options[0],1)} disabled={this.state.able} style={{width: '90%', margin: '2px',textAlign:'center',backgroundColor:this.state.color1,color:'white',borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',opacity:this.state.opacity}}/>
-  <ListItem  primaryText={quiz[this.state.qstnno].options[1]}  onTouchTap={() => this.handleClick(quiz[this.state.qstnno].options[1],2)} disabled={this.state.able} style={{width: '90%', margin: '2px',textAlign:'center',backgroundColor:this.state.color2,color:'white',borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',opacity:this.state.opacity}} />
-  <ListItem  primaryText={quiz[this.state.qstnno].options[2]}  onTouchTap={() => this.handleClick(quiz[this.state.qstnno].options[2],3)} disabled={this.state.able} style={{width: '90%', margin: '2px',textAlign:'center',backgroundColor:this.state.color3,color:'white',borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',opacity:this.state.opacity}} />
-  <ListItem  primaryText={quiz[this.state.qstnno].options[3]}  onTouchTap={() => this.handleClick(quiz[this.state.qstnno].options[3],4)} disabled={this.state.able} style={{width: '90%', margin: '2px',textAlign:'center',backgroundColor:this.state.color4,color:'white',borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',opacity:this.state.opacity}} />
-  <FlatButton label="Next >>" primary={true} onTouchTap={() => this.handleChange([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])} style={{textAlign:'right'}}/>
-
+  {TopicOptions[this.state.questionNumber]}
+  <FlatButton label="Next >>" secondary={true} onTouchTap={() => this.handleChange()} style={{textAlign:'right'}}/>
+  <FlatButton label="Submit" secondary={true}  style={{textAlign:'right'}}/>
   </Col>
   </Row>
   </center>
@@ -335,18 +202,5 @@ render()
   </center>
   );
 }
-}
-function ImageFun(){
-  var imagevar=quiz[qstn_no].image
-  if(imagevar==null){
-    return(
-     <img src="" style={{height:"260px",width:'80%'}}/>
-     );
-  }
-  else{
-    return(
-      <img src={quiz[qstn_no].image} style={{height:"260px",width:'80%'}}/>
-      );
-  }
 }
 export default GamePlay;
--- client/components/Timer.jsx
+++ client/components/Timer.jsx
@@ -1,53 +1,38 @@
 import React from 'react'
 import Paper from 'material-ui/Paper'
 import {redA700, deepOrange400, teal500, blue500, grey900,pinkA200,blue900} from './../../node_modules/material-ui/styles/colors';
-/*-----------------------------------End of importing--------------------------------------------------*/ 
-
-/*-----------------------------------Start Timer progress for count time-------------------------------*/ 
+/*-----------------------------------End of importing--------------------------------------------------*/
+/*-----------------------------------Start Timer progress for count time-------------------------------*/
 const styles2 = {
  height: 15,
- width: 10, 
+ width: 10,
  margin: 3,
  textAlign: 'center',
- display: 'inline-block', 
-
-
+ display: 'inline-block',
 }
 var timer=1000;
 class TimerSpeed extends React.Component
 {
  constructor(props){
-
    super(props);
    this.state = {
      block : this.props.blocks,
      interval : ''
    }
-
-
  }
- 
- componentDidMount() {
-  //console.log(this.props.timelimit);
-  timer=this.props.timelimit/15;
-  console.log(timer);
-    this.setState({
-      interval : setInterval( function() 
-    {
-     //console.log(timer);
-      this.state.block.pop();
 
-      if(this.state.block.length==0)
-      {
-       this.setState({block : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]});
-      }
+ componentDidMount() {
+  timer=this.props.timeLimit;
+    this.setState({
+      interval : setInterval( function()
+    {
+      this.state.block.pop();
       this.setState({block: this.state.block});
     }.bind(this),timer)
   });
  }
-
  componentWillMount() {
-   clearInterval(this.state.intervalId);
+   clearInterval(this.state.interval);
  }
  render(){
    var change = this.state.block.map(function(data) {
