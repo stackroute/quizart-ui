81be4055bca361ab53aaaab112a3f31a05f6bf0d
kirtijalan <kirtijalan194@gmail.com>
2016-12-07 09:08:25 +0530

    neo4j integration

--- controller/signup.js
+++ controller/signup.js
@@ -1,14 +1,27 @@
 var express = require('express'),
 router = express.Router();
+var neo4j = require('neo4j-driver').v1;
 
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "Neo4j"));
+var session = driver.session();
 
 global.userData=[];
 
 console.log("helloKirti")
 
 router.post('/signin',function(req,res){
+
   console.log("in signin js");
   console.log(req.body);
+
+
+  session
+    .run( "CREATE (a:Person {username:{name}, email:{email},password:{pass}}) return a",{name:req.body.userName, email:req.body.email, pass:req.body.password} )
+    .then( function( result ) {
+      console.log("user is created");
+      session.close();
+      driver.close();
+    })
   res.send("end");
 });
 
