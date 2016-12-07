3710d54acfdb07dcdbb188b61a594f2d2605ff85
vishantsharma <vishantsharma213@gmail.com>
2016-12-07 18:02:48 +0530

    Jeopardy Gameplay Integrated

--- client/components/JeopardyGameplay.jsx
+++ client/components/JeopardyGameplay.jsx
@@ -6,7 +6,8 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import {grey900,blue300} from './../../node_modules/material-ui/styles/colors';
 import {ListItem} from 'material-ui/List';
 import Paper from 'material-ui/Paper';
-import {Grid,Row,Col} from 'react-flexbox-grid';
+import MediaQuery from 'react-responsive';
+import {Row,Col} from 'react-flexbox-grid';
 
 
 
@@ -56,7 +57,6 @@ export default class JeopardyGameplay extends React.Component {
 
     changeState()
     {
-
        options = [];
        if(this.state.choice=='')
        {
@@ -64,13 +64,13 @@ export default class JeopardyGameplay extends React.Component {
             for(var i=0;i<topics.options.length;i++)
             {
                 options.push(<button onTouchTap={this.handleOnClick}
-                    style={{width: '50px', height:'10px',margin: '1px',textAlign:'center',
+                    style={{width: '50%', height:'100%',margin: '1px',textAlign:'center',
                     backgroundColor:'#1A237E',color:'white',
-                    padding: '3px 3px 3px 3px',
-                    borderTopLeftRadius:'70px' ,borderTopRightRadius: '70px',
-                    borderBottomLeftRadius: '70px',borderBottomRightRadius: '70px',
+                    padding: '2px 2px 2px 2px',
+                    borderTopLeftRadius:'20px' ,borderTopRightRadius: '20px',
+                    borderBottomLeftRadius: '20px',borderBottomRightRadius: '20px',
                     cursor: 'pointer',
-                    outline: '70px 70px 70px 70px',
+                    outline: '30px 30px 30px 30px',
                     fontSize:'2px'
                 }}>
                 {topics.options[i]}
@@ -82,17 +82,17 @@ export default class JeopardyGameplay extends React.Component {
     this.setState({screen:true});
     this.setState({choice:options});
     // <TimerSpeed timeLimit={1000} blocks={[0,1,2,3,4]} />;
-
 }
 
 render() {
 
     const screenData = this.state.screen ?  
    console.log('options'):
+
     <div>
     <img src="http://res.cloudinary.com/deaxb0msww/image/upload/v1480404062/buzzer_p754xp.png"
     alt="Image Not Available"
-    style={{height:'20px',width:'20px',marginTop:'14px',cursor:'pointer'}}
+    style={{height:'20px',width:'20px',marginTop:'8px',cursor:'pointer'}}
     onTouchTap = {this.changeState} />
     <TimerSpeed timeLimit={1000} blocks={[0,1,2,3,4,5,6,7]} />
     </div>
@@ -103,23 +103,72 @@ render() {
             if(indexQ == this.state.i)
             {
                 return (
-                    <CardTitle titleStyle={{fontSize:'3px',marginTop:-10,lineHeight:'7px'}}
+                    <div>
+        <Row >
+         <Col xs={12} sm={6} md={6} lg={6}>
+                     <MediaQuery minDeviceWidth='1224px'>
+                     <div className="cardTitle">
+                    <CardTitle titleStyle={{fontSize:'3px',width:'65px',margin:'2px',lineHeight:'4px'}}
                     style={{height:10,padding:0}}  
                     title={topics.question}/>
-                    );
+                   
+                </div>
+                </MediaQuery>
+                <MediaQuery maxDeviceWidth='1224px' className="cardTitle">
+                    <CardTitle titleStyle={{fontSize:'1.5px',marginBottom:10,lineHeight:'3px'}}
+                    style={{height:10,padding:0}}  
+                    title={topics.question}/>
+                </MediaQuery>
+                </Col>
+        </Row>
+        </div>
+         );
+
             }
         }
     }));
     return (
         <div>
-        <center>
-        <Card style={{width:'90%',height:'90%',marginTop:-15,backgroundColor:blue300}}>
+        <Row center='xs'>
+         <Col xs={12} sm={6} md={6} lg={6}>
+          {/*<Row center='xs'> */}
+
+          <MediaQuery minDeviceWidth={1} maxDeviceWidth={479}>
+               <div className="some-class">
+        <Card style={{width:'90%',height:'68px',marginTop:'-15px',marginLeft:'2px',backgroundColor:blue300}}>
+        {topicStruct}  
+        {screenData}
+        {this.state.choice}
+        </Card>
+        </div>
+            </MediaQuery>
+            <MediaQuery minDeviceWidth={480} maxDeviceWidth={767}>
+              <div className="some-class">
+        <Card style={{width:'90%',height:'76px',marginTop:'0px',backgroundColor:blue300}}>
         {topicStruct}       
         {screenData}
         {this.state.choice}
-
         </Card>
-        </center>
+        </div>
+            </MediaQuery>
+        <MediaQuery minDeviceWidth={768} maxDeviceWidth={1023}>
+         <div className="some-class">
+        <Card style={{width:'200%',height:'76px',marginTop:'4px', marginLeft:'-15px',backgroundColor:blue300}}>
+        <Row center="xs" style={{fontSize:"15px"}}>{topicStruct} </Row>      
+        {screenData}
+        {this.state.choice}
+        </Card>
+        </div>
+        </MediaQuery>
+        <MediaQuery minDeviceWidth={1024} className="some-class">
+         <Card style={{width:'90%',height:'65px',fontSize:'1px', backgroundColor:blue300, margin:'auto'}}>
+        {topicStruct}       
+        {screenData}
+        {this.state.choice}
+        </Card>
+        </MediaQuery>
+        </Col>
+        </Row>
         </div>
         );
 }
