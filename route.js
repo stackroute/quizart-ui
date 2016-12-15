0844ef17f322f0de2d911e0eff76a6623fb775aa
kirtijalan <kirtijalan194@gmail.com>
2016-12-15 12:07:03 +0530

    integration on one port 8081

--- route.js
+++ route.js
@@ -3,6 +3,36 @@ var bodyParser = require('body-parser');
 var expressJWT = require('express-jwt');
 var jwt = require('jsonwebtoken');
 var app = express();
+var userRoute = require('./controller/index.js');
+var server = require('http').createServer(app);
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var Schema = mongoose.Schema;
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("connected");
+});
+
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = require('morgan');
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const config = require('./webpack.config.js')
+  const compiler = webpack(config)
+
+  app.use(logger('dev'));
+  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }))
+}
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client'))
@@ -13,8 +43,8 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/users/login',require('./controller/index.js'));
-app.use('/',require('./controller/index.js'));
-app.use('/signin',require('./controller/index.js'));
-app.listen('8081',function(){
+app.use('/users/login',userRoute);
+app.use('/',userRoute);
+app.use('/signin',userRoute);
+server.listen('8081',function(){
 });
