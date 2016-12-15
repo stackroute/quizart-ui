6c23cb194892e2b77c4f391bfb918ecd140e50bf
vishantsharma <vishantsharma213@gmail.com>
2016-12-15 16:24:26 +0530

    Addition of server folder

---
+++ server/chat/index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var redis = require('redis');
+var client = redis.createClient();
+
+//var initializeGame = require('../gameManager/initializeGame');
+
+
+
+app.get('/',function(req,res)
+	{
+		res.sendFile(__dirname+"/index.html");
+	});
+
+io.on('connection', function(socket){
+
+  client.get("gameId",function(err,reply)
+  	{
+  		console.log("GameID :"+reply);
+  	});
+  client.get("scores",function(err,reply)
+  {
+  	console.log("Scores :"+reply);
+  });
+  // socket.on('chat message', function(msg){
+  //   //io.emit('chat message', msg);
+  //   socket.broadcast.emit('chat message',msg);
+  // });
+})
+
+http.listen(3050,function()
+{
+	console.log("File served");
+})
