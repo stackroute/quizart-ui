8c841308594a47397fe722e5004dcd248198fd72
Lal Jose <laljosekj@gmail.com>
2016-12-27 17:32:17 +0530

    solved timing issues in jeopardyGamePlay component

--- client/components/CardChild.jsx
+++ client/components/CardChild.jsx
@@ -11,70 +11,70 @@ var openedCards=0;
 
 export default class CardChild extends React.Component
 {
-  constructor(props)
-  {
-    super(props);
-    this.state={view:'points',cName:'NscaledCard',content:this.props.points,question:'Points',posTop:this.props.top,posLeft:this.props.posL,open:false};
-    this.handleClick=this.handleClick.bind(this);
+ constructor(props)
+ {
+   super(props);
+   this.state={view:'points',cName:'NscaledCard',content:this.props.points,question:'Points',posTop:this.props.top,posLeft:this.props.posL,open:false};
+   this.handleClick=this.handleClick.bind(this);
 
-    this.classChange = this.classChange.bind(this);
-  }
+   this.classChange = this.classChange.bind(this);
+ }
 
 
-  classChange()
-  {
-    if(this.state.cName=='ScaledCard')
+ classChange()
+ {
+   if(this.state.cName=='ScaledCard')
+   {
+     this.setState({content:'*'});
+     this.setState({cName:'NscaledCard'});
+   }
+
+
+ }
+ handleClick()
+ {
+   counter++;
+   if(openedCards==29)
+   {
+     alert("opened all the cards");
+   }
+     if(this.state.content=='*')
+     {
+     console.log("state *");
+     }
+     else
+     {
+     if(this.state.cName=='NscaledCard')
+     {
+         this.setState({cName:'ScaledCard'});
+         this.setState({content:<JeopardyGameplay classChange={this.classChange} points={this.props.points} limit={1}/>});
+     }
+     else
+     if(this.state.cName=='ScaledCard' && counter==3)
+     {
+     openedCards++;
+         this.setState({cName:'NscaledCard'});
+     counter=0;
+     this.setState({content:'*'});
+     }
+ }
+
+ }
+    render()
     {
-      this.setState({content:'*'});
-      this.setState({cName:'NscaledCard'});
+      const cardStyle = {
+     width: this.props.cardWidth,
+     height: this.props.cardHeight,
+     marginLeft:15,
+     marginTop:7,
+     display: 'inline-block',
+     textAlign: "center",
+     top: '44%',
+     left :'44%',
+     backgroundColor: '#0E0983 ',
+   }
+        return(
+            <Card onClick={this.handleClick} className={this.state.cName} style={cardStyle} >  <h4 className={this.state.question} style={{color:"#F4D03F " ,marginTop:17}} >{this.state.content}</h4></Card>
+            );
     }
-
-
-  }
-  handleClick()
-  {
-    counter++;
-    if(openedCards==29)
-    {
-      alert("opened all the cards");
-    }
-  	if(this.state.content=='*')
-  	{
-      console.log("state *");
-  	}
-  	else
-  	{
-  	if(this.state.cName=='NscaledCard')
-  	{
-  		this.setState({cName:'ScaledCard'});
-  		this.setState({content:<JeopardyGameplay classChange={this.classChange} points={this.props.points} limit={1}/>});
-  	}
-  	else
-      if(this.state.cName=='ScaledCard' && counter==3)
-  	{
-      openedCards++;
-  		this.setState({cName:'NscaledCard'});
-      counter=0;
-      this.setState({content:'*'});
-  	}
-  }
-
-  }
-	render()
-	{
-	  const cardStyle = {
-      width: this.props.cardWidth,
-      height: this.props.cardHeight,
-      marginLeft:15,
-      marginTop:7,
-      display: 'inline-block',
-      textAlign: "center",
-      top: '44%',
-      left :'44%',
-      backgroundColor: '#0E0983',
-    }
-		return(
-			<Card onClick={this.handleClick} className={this.state.cName} style={cardStyle} >  <h4 className={this.state.question} style={{color:"#F4D03F" ,marginTop:17}} >{this.state.content}</h4></Card>
-			);
-	}
 }
