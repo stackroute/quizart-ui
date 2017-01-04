ee1c416a0656d0a242c1fe1e0f196d3678e249cb
vishantsharma <vishantsharma213@gmail.com>
2017-01-04 20:26:51 +0530

    Dashboard And Waiting Screen Responsive

--- client/views/DashboardView.jsx
+++ client/views/DashboardView.jsx
@@ -30,26 +30,28 @@ export default class DashboardView extends React.Component {
                   <Col xs={6} sm={6} md={6} lg={6}>
                 <Link to = '/waitingForPlayers'><RaisedButton
                   backgroundColor="#42C299"
-                  label="Play Jeopardy!"
+                  label="Play Jeopardy"
                   labelColor="white"
                   buttonStyle={{height:'60px', width:'200px', borderRadius:'10px'}}
                   />
                 </Link>
               </Col>
               </Row>}
-              overlayContainerStyle={{bottom:'25%'}}
+              overlayContainerStyle={{bottom:'35%'}}
               overlayContentStyle={{backgroundColor: 'rgba(0,0,0,0)'}}
 
             >
-            <MediaQuery minDeviceWidth='1224px'>
-         <div className="some-class">
-           <img src="http://res.cloudinary.com/deaxb0msww/image/upload/v1483453570/Dboard_jtxczc.jpg" style={styles.imageStyle}/>
-         </div>
-         </MediaQuery>
-         <MediaQuery maxDeviceWidth='1224px' className="some-class">
-         <img src="http://res.cloudinary.com/deaxb0msww/image/upload/v1483463322/DboardRes_l4vzyw.jpg" style={styles.imageStyle}/>
-         </MediaQuery>
-            </CardMedia>
+              <MediaQuery minDeviceWidth='700px'>
+              <div className="some-class">
+                <img src="http://res.cloudinary.com/deaxb0msww/image/upload/v1483453570/Dboard_jtxczc.jpg" style={styles.imageStyle}/>
+              </div>
+              </MediaQuery>
+              <MediaQuery maxDeviceWidth='699px' className="some-class">
+              <img src="http://res.cloudinary.com/deaxb0msww/image/upload/v1483463322/DboardRes_l4vzyw.jpg" style={styles.imageStyle}/>
+              </MediaQuery>
+
+
+              </CardMedia>
 
         </Card>
       </div>
